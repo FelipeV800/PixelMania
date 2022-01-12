df5d0f33518514d089d07ca73d7a615daf0f378e
Felipe Vasquez <vasquezruizcarlos@gmail.com>
2022-01-12 12:58:24 -0500

    propiedades para dibujar con el mouse agregadas

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var papel = workbench.getContext("2d");
 var x = 250;
 var y = 250;
 
-dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+dibujarLinea("red", x - 1, y - 1, x + 1, y + 1, papel);
+dibujarLinea("grey", 0, 0, 500, 0, papel)
+dibujarLinea("grey", 500, 0, 500, 500, papel)
+dibujarLinea("grey", 500, 500, 0, 500, papel)
+dibujarLinea("grey", 0, 500, 0, 0, papel)
 
-function dibujarLinea (color, x_inicial, y_inicial, x_final, y_final, lienzo) {
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo) {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
     lienzo.lineWidth = 3;
@@ -17,8 +21,15 @@ function dibujarLinea (color, x_inicial, y_inicial, x_final, y_final, lienzo) {
     lienzo.closePath();
 }
 
-function dibujar (properties) {
-    var colorDibujo = "red";
-    var movimiento = 10;
-    console.log(properties);
+function dibujar(properties) {
+    if (properties.button == 0) {
+        var colorDibujo = "blue";
+        dibujarLinea(colorDibujo, x, y, properties.layerX, properties.layerY, papel);
+        y = properties.layerY;
+        x = properties.layerX;
+    } else if (properties.button == 1) {
+        x = properties.layerX;
+        y = properties.layerY;
+        dibujarLinea("pink", x - 1, y - 1, x + 1, y + 1, papel);
+    }
 }
