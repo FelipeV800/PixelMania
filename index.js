923693de4d8c48dbeec1c72459aecaf5b2415a1e
Felipe Vasquez <vasquezruizcarlos@gmail.com>
2022-01-12 12:04:30 -0500

    funccion Dibujar Linea y Canvas creado

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 document.addEventListener("mousedown", dibujar);
 
-function dibujar () {
-    alert("it works!!");
+var workbench = document.getElementById("espacioDibujo");
+var papel = workbench.getContext("2d");
+var x = 250;
+var y = 250;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+
+function dibujarLinea (color, x_inicial, y_inicial, x_final, y_final, lienzo) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(x_inicial, y_inicial);
+    lienzo.lineTo(x_final, y_final);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujar (properties) {
+    var colorDibujo = "red";
+    var movimiento = 10;
+    console.log(properties);
 }
