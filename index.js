3e804cee7233c88f63db91b3a7ac40f1670af058
Felipe Vasquez <vasquezruizcarlos@gmail.com>
2022-01-12 13:24:51 -0500

    funcion para dibujar solo con el click presionado

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-document.addEventListener("mousedown", dibujar);
+document.addEventListener("mousedown", initDibujo);
+document.addEventListener("mouseup", stopDibujo)
+document.addEventListener("mousemove", dibujar)
+document.addEventListener("wheel", cambiarGrosor)
 
 var workbench = document.getElementById("espacioDibujo");
 var papel = workbench.getContext("2d");
+var activation = false;
 var x = 250;
 var y = 250;
+var grosor = 3;
 
-dibujarLinea("red", x - 1, y - 1, x + 1, y + 1, papel);
-dibujarLinea("grey", 0, 0, 500, 0, papel)
-dibujarLinea("grey", 500, 0, 500, 500, papel)
-dibujarLinea("grey", 500, 500, 0, 500, papel)
-dibujarLinea("grey", 0, 500, 0, 0, papel)
+dibujarLinea("red", 3, x - 1, y - 1, x + 1, y + 1, papel);
+dibujarLinea("grey", 3, 0, 0, 500, 0, papel)
+dibujarLinea("grey", 3, 500, 0, 500, 500, papel)
+dibujarLinea("grey", 3, 500, 500, 0, 500, papel)
+dibujarLinea("grey", 3, 0, 500, 0, 0, papel)
 
-function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo) {
+function dibujarLinea(color, grosor, x_inicial, y_inicial, x_final, y_final, lienzo) {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
-    lienzo.lineWidth = 3;
+    lienzo.lineWidth = grosor;
     lienzo.moveTo(x_inicial, y_inicial);
     lienzo.lineTo(x_final, y_final);
     lienzo.stroke();
     lienzo.closePath();
 }
 
-function dibujar(properties) {
-    if (properties.button == 0) {
+function dibujar(properties){
+    if (activation == true) {
         var colorDibujo = "blue";
-        dibujarLinea(colorDibujo, x, y, properties.layerX, properties.layerY, papel);
-        y = properties.layerY;
-        x = properties.layerX;
-    } else if (properties.button == 1) {
-        x = properties.layerX;
-        y = properties.layerY;
-        dibujarLinea("pink", x - 1, y - 1, x + 1, y + 1, papel);
+        dibujarLinea(colorDibujo, grosor, x, y, properties.layerX, properties.layerY, papel);
     }
+    y = properties.layerY;
+    x = properties.layerX;
+}
+
+function initDibujo(properties) {
+    if(properties.button == 0) {
+        activation = true;
+        y = properties.layerY;
+        x = properties.layerX;
+    }
+}
+
+function stopDibujo(properties) {
+    activation = false;
+    y = properties.layerY;
+    x = properties.layerX;
+}
+
+function cambiarGrosor(properties) {
+    console.log(properties.deltaY);
 }
